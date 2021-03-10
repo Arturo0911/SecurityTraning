47c76884e4a1557bf1af14bae14c2259fa3b0593
Arturo Negreiros <anegreiross@outlook.com>
2021-03-10 17:51:03 -0500

    parsed data

--- newScripts/utilities/Scrap.py
+++ newScripts/utilities/Scrap.py
@@ -27,21 +27,20 @@ class Scrap:
             findAll(tag, attributes, recursive, text, limit, keywords)
             find(tag, attributes, recursive, text, keywords)
         """
-        try:
-            with urlopen(url) as f:
+    
+        with urlopen(url) as f:
 
-                self.base = BeautifulSoup(f,"html.parser")
-                #print(self.base.findAll('a'))
-                self.data_parsed = self.base.findAll('span')
-                if self.data_parsed is None:
-                    return None
-                else:
-                    for x in self.data_parsed:
-                        print(x.get_text())
-        except HTTPError as e:
-            return str(e)
+            self.base = BeautifulSoup(f,"html.parser")
+        
+            #print(self.base.findAll('a'))
+            self.data_parsed = self.base.findAll('a')
+            #print(self.data_parsed)    
+            for x in self.data_parsed:
 
-
+                if x.get_text() != "":
+                
+                    print(x.get_text(), "his type")
+                    
     def initializr(self):
         
         try:
@@ -52,12 +51,13 @@ class Scrap:
             for o,a in opt:
 
                 if o in ('-u', '--url'):
+                    self.find_into_url(a)
                     
-                    if self.find_into_url(a) is None:
+                    """if self.find_into_url(a) is None:
                         print("Data cannot be founded")
                     else:
                         self.find_into_url(a)
-                        print("[*]  ...Finished")
+                        print("[*]  ...Finished")"""
 
                 if o in ('-h', '--help'):
                     self._usage()
