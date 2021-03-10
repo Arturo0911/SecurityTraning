ef4f9b5daa3c7cb7f65ac1d1e0b67be56e579c12
Arturo Negreiros <anegreiross@outlook.com>
2021-03-10 18:38:07 -0500

    Complete the Error in 403 Forbbiden whenever use the urlopen in some pages

--- newScripts/utilities/Scrap.py
+++ newScripts/utilities/Scrap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from urllib.request import urlopen
+from urllib.request import Request
 from urllib.error import HTTPError
 import getopt
 import sys
@@ -19,6 +20,13 @@ class Scrap:
     def _usage(self):
         print("The goal of this project is get any tag information about the web page scanned ")
         print("The tag -u or --url you must to include the url to scan <https://wherever.com>  ")
+    
+
+    def _errors(self):
+        print(""" Using urllib.request.urlopen() to open a website when crawling, and encounters “HTTP Error 403: Forbidden”. 
+                It possibly due to the server does not know the request is coming from. Some websites will verify the UserAgent 
+                in order to prevent from abnormal visit. So you should provide information of your fake browser visit.""")
+
 
     def find_into_url(self, url):
         
@@ -27,25 +35,49 @@ class Scrap:
             findAll(tag, attributes, recursive, text, limit, keywords)
             find(tag, attributes, recursive, text, keywords)
         """
-    
-        with urlopen(url) as f:
+        try: 
+            with urlopen(url) as f:
 
-            self.base = BeautifulSoup(f,"html.parser")
-        
-            #print(self.base.findAll('a'))
-            self.data_parsed = self.base.findAll('a')
-            #print(self.data_parsed)    
-            for x in self.data_parsed:
+                self.base = BeautifulSoup(f,"html.parser")
+                #print(self.base.findAll('a'))
+                self.data_parsed = self.base.findAll('span')
+                #print(self.data_parsed)    
+                for x in self.data_parsed:
 
-                if x.get_text() != "":
-                
-                    print(x.get_text(), "his type")
+                    if x.get_text() != "":
                     
+                        print(x.get_text(), "his type")
+        except HTTPError as e:
+
+            if str(e) == "HTTP Error 403: Forbidden":
+                self._errors()
+            else:
+                print(str(e))
+    
+    def show_user_agent(self, url):
+
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
+                      'AppleWebKit/537.11 (KHTML, like Gecko) '
+                      'Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
+            request = Request(url = url, headers = headers)
+            with urlopen(request).read() as f:
+                print(f.reead(100).decode('utf-8'))
+
+        except Exception as e:
+            print(str(e))
+
+
     def initializr(self):
         
         try:
             
-            opt, args = getopt.getopt(self.args, "hu:",["help", "url="])
+            opt, args = getopt.getopt(self.args, "hu:a:",["help", "url=", "agent="])
             #print(opt)
 
             for o,a in opt:
@@ -62,12 +94,11 @@ class Scrap:
                 if o in ('-h', '--help'):
                     self._usage()
 
+                if o in ('-a', '--agent'):
+                    self.show_user_agent(a)
+
         except getopt.error as e:
             print(str(e))
-
-
-
-
 
 
 if __name__ == '__main__':
